81b3b5d1d7457cc44ca68e3e00c2465f747e2e95
uday-gandhi04 <udaygandhi2004@gmail.com>
2025-07-12 19:26:07 +0530

    Fix seeding command in render-build script to remove incorrect file extension

--- travel_site/render-build.sh
+++ travel_site/render-build.sh
@@ -8,7 +8,7 @@ pip install -r requirements.txt
 
 # Run migrations
 python manage.py migrate
-python manage.py seed_travel.py
+python manage.py seed_travel
 
 # Optional: You can create a default admin user here (if needed)
 # echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')" | python manage.py shell
