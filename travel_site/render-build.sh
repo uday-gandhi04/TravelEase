1e1600a83cd4e3fab26eba1a842919367be249a9
uday-gandhi04 <udaygandhi2004@gmail.com>
2025-07-12 19:22:13 +0530

    Update render-build script to include seeding command for travel data

--- travel_site/render-build.sh
+++ travel_site/render-build.sh
@@ -12,3 +12,4 @@ python manage.py seed_travel.py
 
 # Optional: You can create a default admin user here (if needed)
 # echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')" | python manage.py shell
+python manage.py collectstatic --noinput
